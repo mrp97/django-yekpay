a5d8f61c8e7d4242c27387d4d2f26ea35a9a7c24
glyphack <sh.hooshyari@gmail.com>
2018-10-20 21:29:46 +0330

    added created and status fields to db

--- django_yekpay/models.py
+++ django_yekpay/models.py
@@ -5,19 +5,21 @@ from django.db import models
 #from model_utils.models import TimeStampedModel
 
 
-class transactions(models.Model):
+class Transaction(models.Model):
     amount = models.DecimalField(max_digits=64, decimal_places=2, default=0, blank=True, null=True)
-    description = models.textField()
-    from_currency_code = models.integerField()
-    to_currency_code = models.integerField()
-    first_name = models.charField(max_length=225)
-    last_name = models.charField(max_length=225)
-    email = models.charField(max_length=225)
-    mobile = models.charField(max_length=225)
-    address = models.charField(max_length=225)
-    country = models.charField(max_length=225)
-    postal_code= models.charField(max_length=225)
-    city = models.charField(max_length=100)
+    description = models.TextField()
+    from_currency_code = models.IntegerField()
+    to_currency_code = models.IntegerField()
+    first_name = models.CharField(max_length=225)
+    last_name = models.CharField(max_length=225)
+    email = models.CharField(max_length=225)
+    mobile = models.CharField(max_length=225)
+    address = models.CharField(max_length=225)
+    country = models.CharField(max_length=225)
+    postal_code= models.CharField(max_length=225)
+    city = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=100)
 
     def __repr__(self):
         return '<yekpay id:{0}>'.format(self.id)
