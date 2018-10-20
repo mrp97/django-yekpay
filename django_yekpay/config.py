a394cb171172f69a591cf3fe8efc3606c8454854
glyphack <sh.hooshyari@gmail.com>
2018-10-20 21:27:43 +0330

    added empty line to config.py

---
+++ django_yekpay/config.py
@@ -0,0 +1,3 @@
+YEKPAY_START_GATEWAY  = 'https://gate.yekpay.com/api/payment/start/'
+YEKPAY_VERIFY_GATEWAY =  'https://gate.yekpay.com/api/payment/verify'
+YEKPAY_REQUEST_GATEWAY = 'https://gate.yekpay.com/api/payment/request'
