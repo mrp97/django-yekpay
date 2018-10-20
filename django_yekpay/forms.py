3a5f35ccffeb96b49181c7b15d027e7096847279
glyphack <sh.hooshyari@gmail.com>
2018-10-20 21:31:29 +0330

    added yekpay form to save transaction data

---
+++ django_yekpay/forms.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django import forms
+
+from .models import Transaction
+class YekpayTransactionForm(forms.Form):
+    class Meta:
+        model = Transaction
+        exclude = ['created_at']
