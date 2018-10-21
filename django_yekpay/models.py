b1c80732f9d0cd8fdd97b3c53424b0cb8759f8a2
glyphack <sh.hooshyari@gmail.com>
2018-10-21 16:25:06 +0330

    made field names like yekpay's standard

--- django_yekpay/models.py
+++ django_yekpay/models.py
@@ -9,16 +9,16 @@ class Transaction(models.Model):
     amount = models.DecimalField(max_digits=64, decimal_places=2, default=0, blank=True, null=True)
     authority = models.IntegerField() #by module
     description = models.TextField()
-    from_currency_code = models.IntegerField()
-    to_currency_code = models.IntegerField()
-    first_name = models.CharField(max_length=225)
-    last_name = models.CharField(max_length=225)
+    fromCurrencyCode = models.IntegerField()
+    toCurrencyCode = models.IntegerField()
+    firstName = models.CharField(max_length=225)
+    lastName = models.CharField(max_length=225)
     email = models.CharField(max_length=225)
     mobile = models.CharField(max_length=225)
     orderNumber = models.CharField(max_length=500)
     address = models.CharField(max_length=225)
     country = models.CharField(max_length=225)
-    postal_code= models.CharField(max_length=225)
+    postalCode= models.CharField(max_length=225)
     city = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True) #by module
     status = models.CharField(max_length=100) # by module
