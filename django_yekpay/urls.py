aaec240ab418bb241c4e52cae9c215b9c90db6e7
glyphack <sh.hooshyari@gmail.com>
2018-10-21 17:49:17 +0330

    removed urls

--- django_yekpay/urls.py
+++ django_yekpay/urls.py
@@ -7,29 +7,29 @@ from . import views
 
 app_name = 'django_yekpay'
 urlpatterns = [
-    url(
-        regex="^transactions/~create/$",
-        view=views.transactionsCreateView.as_view(),
-        name='transactions_create',
-    ),
-    url(
-        regex="^transactions/~verify/$",
-        view=views.verifyTransaction.as_view(),
-        name='transactions_verify',
-    ),
-    url(
-        regex="^transactions/(?P<pk>\d+)/$",
-        view=views.transactionsDetailView.as_view(),
-        name='transactions_detail',
-    ),
+    # url(
+    #     regex="^transactions/~create/$",
+    #     view=views.yekpay_start_transaction(transaction_data),
+    #     name='transactions_create',
+    # ),
+    # url(
+    #     regex="^transactions/~verify/$",
+    #     view=views.yekpay_proccess_transaction(),
+    #     name='transactions_verify',
+    # ),
+    # url(
+    #     regex="^transactions/(?P<pk>\d+)/$",
+    #     view=views.transactionsDetailView.as_view(),
+    #     name='transactions_detail',
+    # ),
     # url(
     #     regex="^transactions/(?P<pk>\d+)/~update/$",
     #     view=views.transactionsUpdateView.as_view(),
     #     name='transactions_update',
     # ),
-    url(
-        regex="^transactions/$",
-        view=views.transactionsListView.as_view(),
-        name='transactions_list',
-    ),
+    # url(
+    #     regex="^transactions/$",
+    #     view=views.transactionsListView.as_view(),
+    #     name='transactions_list',
+    # ),
 	]
