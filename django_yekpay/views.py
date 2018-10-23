1ba1e6f4f6da2d6784af57b36cfcbcbb54b9a0f5
glyphack <sh.hooshyari@gmail.com>
2018-10-23 16:34:17 +0330

    moved to utils.py

--- django_yekpay/views.py
+++ django_yekpay/views.py
@@ -8,70 +8,13 @@ from django.views.generic import (
     ListView
 )
 
-from django.shortcuts import HttpResponseRedirect
-from django.conf import settings
-
-from .models import (
-    Transaction,
-)
-from .config import *
-
-# constants
-MERCHANTID = getattr(settings, 'YEKPAY_MERCHANT_ID', '')
 
 
-def yekpay_start_transaction(transaction_data):
-    global MERCHANTID
-
-    config = {
-        "merchantId": MERCHANTID,
-        "callback": getattr(settings, 'YEKPAY_CALLBACK_URL', ''),
-        'orderNumber': time()
-    }
-    data = {**config, **transaction_data}
-
-    json_data = json.dumps(data)
-    headers = {'Content-Type': 'application/json', 'Content-Length': str(len(json_data))}
-    response = requests.post(YEKPAY_REQUEST_GATEWAY, headers=headers, data=json_data)
-    authority = dict(json.loads(response.text))
-    if authority['Code'] == 100:
-        print("redirecting to yekpay's gateway")
-        transaction = Transaction(authority=authority['Authority'], status='pending', **transaction_data)
-        transaction.save()
-        return (YEKPAY_START_GATEWAY + str(authority['Authority']))
-    else:
-        print('django_yekpay error' + str(authority['Description']) + str(authority['Code']))
 
 
-def yekpay_proccess_transaction(request):
-    global MERCHANTID
-    verify_transaction_data = {
-        "merchantId": MERCHANTID,
-        "authority": request.GET['authority']
-    }
-    json_data = json.dumps(verify_transaction_data)
-    headers = {'Content-Type': 'application/json', 'Content-Length': str(len(json_data))}
-    response = requests.post(YEKPAY_VERIFY_GATEWAY, headers=headers, data=json_data)
-    trans_status = dict(json.loads(response.text))
-    if 'Code' in trans_status:
-        if trans_status['Code'] == 100:
-            # transaction_succeed
 
-            transaction = Transaction.objects.get(orderNumber=trans_status['OrderNo'])
-            transaction.status = 'success'
-            transaction.save(update_fields=['status'])
-            return True
 
-        else:
-            # transaction_failed
-            transaction = Transaction.objects.get(orderNumber=trans_status['OrderNo'])
-            transaction.status = 'failed'
-            transaction.save(update_fields=['status'])
-            logging.info(trans_status)
-            return False
-    else:
-        logging.error(trans_status)
-        return 'there was a problem in payment'
+
 
 # class transactionsDetailView(DetailView):
 #     model = transactions
