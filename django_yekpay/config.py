c7e88d65afe47edf04f1e65886e31116fe931608
glyphack <sh.hooshyari@gmail.com>
2018-10-23 16:35:18 +0330

    added currency codes and transaction status codes

--- django_yekpay/config.py
+++ django_yekpay/config.py
@@ -1,3 +1,48 @@
 YEKPAY_START_GATEWAY  = 'https://gate.yekpay.com/api/payment/start/'
 YEKPAY_VERIFY_GATEWAY =  'https://gate.yekpay.com/api/payment/verify'
 YEKPAY_REQUEST_GATEWAY = 'https://gate.yekpay.com/api/payment/request'
+CURRENCY_CODES = {
+    'EUR': 978, #Euro
+    'IRR': 364, #Iranian Rial
+    'CHF': 756, #Swiss Franc
+    'AED': 784, #United Arab Emirates Dirham
+    'CNY': 156, #Chinese Yuan
+    'GBP': 826, #British Pound
+    'JPY': 392, #Japanese 100 Yens
+    'RUB': 643, #Russian Ruble
+    'TRY': 949, #Turkish New Lira
+}
+
+CURRENCY_CHOICES = (
+    ('EUR', 'Euro'),
+    ('IRR', 'Iranian Rial'),
+    ('CHF', 'Swiss Franc'),
+    ('AED', 'United Arab Emirates Dirham'),
+    ('CNY', 'Chinese Yuan'),
+    ('GBP', 'British Pound'),
+    ('JPY', 'Japanese 100 Yens'),
+    ('RUB', 'Russian Ruble'),
+    ('TRY', 'Turkish New Lira'),
+)
+TRANSACTION_STATUS_CODES = {
+    -1: 'INCOMPLETE_PARAMETERS',
+    -2: 'MERCHANT_CODE_INCORRECT',
+    -3: 'MERCHANT_CODE_NOT_ACTIVE',
+    -8: 'AUTHORITY_CODE_INVALID',
+    -9: 'ALREADY_VERIFIED',
+    -10: 'FAILDED',
+    -100: 'UNKNOWN',
+    100: 'SUCCESS'
+}
+
+TRANSACTION_STATUS_CHIOCES = (
+    ('PENDING','Transaction has just started'),
+    ('INCOMPLETE_PARAMETERS', 'Transaction parameters were incomplete'),
+    ('MERCHANT_CODE_INCORRECT','Merchant code was incorrect'),
+    ('MERCHANT_CODE_NOT_ACTIVE','Merchant code is not active'),
+    ('AUTHORITY_CODE_INVALID', 'Authority code was invalid'),
+    ('ALREADY_VERIFIED', 'Transaction was already verfied'),
+    ('FAILDED', 'Transaction was failed'),
+    ('UNKNOWN', 'Transaction was failed with unknown error'),
+    ('SUCCESS','Transaction was successfully done'),
+)
