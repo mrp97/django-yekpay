008adccc2f31440ec3a69dc77bd921ffdb4ce63c
glyphack <sh.hooshyari@gmail.com>
2018-10-28 19:32:22 +0330

    redirect user to callback url

--- django_yekpay/views.py
+++ django_yekpay/views.py
@@ -2,7 +2,7 @@
 import requests, json, logging, uuid
 from time import time
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from .models import Transaction
 from django.views.generic import (
     UpdateView,
@@ -23,14 +23,14 @@ def success(request):
     transaction.authorityVerify = str(request.GET['authority'])
     transaction.status = 'SUCCESS'
     transaction.save(update_fields=['authorityVerify','Status'])
-    return HttpResponse(yekpay_process_transaction(request))
+    return redirect(transaction.redirect_url)
 
 def fail(request):
     transaction = Transaction.objects.get(authorityStart=str(request.GET['authoritys']))
     transaction.authorityVerify = str(request.GET['authority'])
     transaction.status = 'FAILED'
     transaction.save(update_fields=['authorityVerify', 'Status'])
-    yekpay_process_transaction(request)
+    return redirect(transaction.redirect_url)
 
 # class transactionsDetailView(DetailView):
 #     model = transactions
