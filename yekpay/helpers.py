d3b24fb6477c6353d052875adc09e0cf1fd61ec5
glyphack <sh.hooshyari@gmail.com>
2019-02-24 17:21:18 +0330

    [middlewares]: __init__ created

--- yekpay/helpers.py
+++ yekpay/helpers.py
@@ -1,39 +1,42 @@
 # -*- coding: utf-8 -*-
-import  logging
-from time import time
-from django.conf import settings
+import logging
 
-from .models import (
-    Transaction,
-)
+from .models import Transaction
 from .config import *
-from .exceptions import *
-from .request_utils import ( request_yekpay, request_yekpay_start,
-    request_yekpay_verify, request_yekpay_start_simulation,
-    request_yekpay_verify_simulation )
-from .utils import ( convert_status_code_to_string,
-    convert_currency_to_currency_code, get_call_back_url,
-    generate_yekpay_start_transaction_data, process_transaction_trans_status )
+from .request_utils import (
+    request_yekpay_start,
+    request_yekpay_verify,
+    request_yekpay_start_simulation,
+    request_yekpay_verify_simulation,
+)
+
+from .utils import (
+    generate_yekpay_start_transaction_data,
+    process_transaction_trans_status,
+    get_transaction_from_trans_status,
+)
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-def yekpay_start_transaction(transaction_data,request_function=request_yekpay_start):
+def yekpay_start_transaction(transaction_data, request_function=request_yekpay_start):
     if YEKPAY_SIMULATION:
         request_function = request_yekpay_start_simulation
     transaction = Transaction.objects.create_transaction(transaction_data)
     start_transaction_data = generate_yekpay_start_transaction_data(transaction)
-    logging.info('starting transaction'+ transaction.order_number.hashid )
-    yekpay_response_data = request_function(
-        data= start_transaction_data
-    )
-    if yekpay_response_data['Code'] == 100:
+    logging.info("starting transaction" + transaction.order_number.hashid)
+    yekpay_response_data = request_function(data=start_transaction_data)
+    if yekpay_response_data["Code"] == 100:
         logging.info("returning redirecting url to yekpay's gateway")
-        transaction.authority_start = str(yekpay_response_data['Authority'])
-        transaction.save(update_fields=['authority_start'])
+        transaction.authority_start = str(yekpay_response_data["Authority"])
+        transaction.save(update_fields=["authority_start"])
         return transaction
     else:
-        logging.error('django_yekpay error' + str(yekpay_response_data['Description']) + str(yekpay_response_data['Code']))
+        logging.error(
+            "django_yekpay error"
+            + str(yekpay_response_data["Description"])
+            + str(yekpay_response_data["Code"])
+        )
         return None
 
 
@@ -41,14 +44,9 @@ def yekpay_process_transaction(request, request_function=request_yekpay_verify):
     if YEKPAY_SIMULATION:
         request_function = request_yekpay_verify_simulation
     trans_status = request_function(
-        data=  {
-            "merchantId": MERCHANTID,
-            "authority": request.GET['authority']
-        }
+        data={"merchantId": MERCHANTID, "authority": request.GET["authority"]}
     )
-    transaction = get_transaction_from_trans_status(
-        trans_status
-    )
+    transaction = get_transaction_from_trans_status(trans_status)
     if transaction:
         transaction.authority_verfiy = request.GET['authority']
         trasaction.save(update_fields='authority_verify')
