8c6e045769276e1f08c00f9bfe1e3975c83bd92f
glyphack <sh.hooshyari@gmail.com>
2019-02-26 15:24:19 +0330

    [yekpay]: refactored PEP8

--- yekpay/config.py
+++ yekpay/config.py
@@ -1,56 +1,54 @@
 from django.conf import settings
 
-MERCHANTID = getattr(settings, 'YEKPAY_MERCHANT_ID', '')
-YEKPAY_SIMULATION = getattr(settings, 'YEKPAY_SIMULATION', False)
-YEKPAY_START_GATEWAY  = 'https://gate.yekpay.com/api/payment/start/'
-YEKPAY_VERIFY_GATEWAY =  'https://gate.yekpay.com/api/payment/verify'
-YEKPAY_REQUEST_GATEWAY = 'https://gate.yekpay.com/api/payment/request'
+MERCHANTID = getattr(settings, "YEKPAY_MERCHANT_ID", "")
+YEKPAY_SIMULATION = getattr(settings, "YEKPAY_SIMULATION", False)
+YEKPAY_START_GATEWAY = "https://gate.yekpay.com/api/payment/start/"
+YEKPAY_VERIFY_GATEWAY = "https://gate.yekpay.com/api/payment/verify"
+YEKPAY_REQUEST_GATEWAY = "https://gate.yekpay.com/api/payment/request"
 CURRENCY_CODES = {
-    'EUR': 978, #Euro
-    'IRR': 364, #Iranian Rial
-    'CHF': 756, #Swiss Franc
-    'AED': 784, #United Arab Emirates Dirham
-    'CNY': 156, #Chinese Yuan
-    'GBP': 826, #British Pound
-    'JPY': 392, #Japanese 100 Yens
-    'RUB': 643, #Russian Ruble
-    'TRY': 949, #Turkish New Lira
+    "EUR": 978,  # Euro
+    "IRR": 364,  # Iranian Rial
+    "CHF": 756,  # Swiss Franc
+    "AED": 784,  # United Arab Emirates Dirham
+    "CNY": 156,  # Chinese Yuan
+    "GBP": 826,  # British Pound
+    "JPY": 392,  # Japanese 100 Yens
+    "RUB": 643,  # Russian Ruble
+    "TRY": 949,  # Turkish New Lira
 }
 
 CURRENCY_CHOICES = (
-    ('EUR', 'Euro'),
-    ('IRR', 'Iranian Rial'),
-    ('CHF', 'Swiss Franc'),
-    ('AED', 'United Arab Emirates Dirham'),
-    ('CNY', 'Chinese Yuan'),
-    ('GBP', 'British Pound'),
-    ('JPY', 'Japanese 100 Yens'),
-    ('RUB', 'Russian Ruble'),
-    ('TRY', 'Turkish New Lira'),
+    ("EUR", "Euro"),
+    ("IRR", "Iranian Rial"),
+    ("CHF", "Swiss Franc"),
+    ("AED", "United Arab Emirates Dirham"),
+    ("CNY", "Chinese Yuan"),
+    ("GBP", "British Pound"),
+    ("JPY", "Japanese 100 Yens"),
+    ("RUB", "Russian Ruble"),
+    ("TRY", "Turkish New Lira"),
 )
 TRANSACTION_STATUS_CODES = {
-    -1: 'INCOMPLETE_PARAMETERS',
-    -2: 'MERCHANT_CODE_INCORRECT',
-    -3: 'MERCHANT_CODE_NOT_ACTIVE',
-    -8: 'AUTHORITY_CODE_INVALID',
-    -9: 'ALREADY_VERIFIED',
-    -10: 'FAILED',
-    -100: 'UNKNOWN',
-    100: 'SUCCESS'
+    -1: "INCOMPLETE_PARAMETERS",
+    -2: "MERCHANT_CODE_INCORRECT",
+    -3: "MERCHANT_CODE_NOT_ACTIVE",
+    -8: "AUTHORITY_CODE_INVALID",
+    -9: "ALREADY_VERIFIED",
+    -10: "FAILED",
+    -100: "UNKNOWN",
+    100: "SUCCESS",
 }
 
-TRANSACTION_STATUS_CHIOCES = (
-    ('PENDING','Transaction has just started'),
-    ('INCOMPLETE_PARAMETERS', 'Transaction parameters were incomplete'),
-    ('MERCHANT_CODE_INCORRECT','Merchant code was incorrect'),
-    ('MERCHANT_CODE_NOT_ACTIVE','Merchant code is not active'),
-    ('AUTHORITY_CODE_INVALID', 'Authority code was invalid'),
-    ('ALREADY_VERIFIED', 'Transaction was already verified'),
-    ('FAILED', 'Transaction was failed'),
-    ('UNKNOWN', 'Transaction was failed with unknown error'),
-    ('SUCCESS','Transaction was successfully done'),
+TRANSACTION_STATUS_CHOICES = (
+    ("PENDING", "Transaction has just started"),
+    ("INCOMPLETE_PARAMETERS", "Transaction parameters were incomplete"),
+    ("MERCHANT_CODE_INCORRECT", "Merchant code was incorrect"),
+    ("MERCHANT_CODE_NOT_ACTIVE", "Merchant code is not active"),
+    ("AUTHORITY_CODE_INVALID", "Authority code was invalid"),
+    ("ALREADY_VERIFIED", "Transaction was already verified"),
+    ("FAILED", "Transaction was failed"),
+    ("UNKNOWN", "Transaction was failed with unknown error"),
+    ("SUCCESS", "Transaction was successfully done"),
 )
 
-FAILURE_REASONS = [
-    'test transaction failed',
-]
+FAILURE_REASONS = ["test transaction failed"]
