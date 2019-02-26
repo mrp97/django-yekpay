011ea1857b7ae9f25d0758008a7e82b61ff981ef
glyphack <sh.hooshyari@gmail.com>
2019-02-26 14:33:34 +0330

    [yekapy]: refactored PEP8

--- yekpay/schema.py
+++ yekpay/schema.py
@@ -4,54 +4,46 @@ from graphene import relay
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
-from .config import TRANSACTION_STATUS_CHIOCES
 from .models import Transaction
-from .helpers import yekpay_start_transaction
-from .request_utils import request_yekpay_start_simulation
 
 
 class TransactionNode(DjangoObjectType):
     class Meta:
         model = Transaction
-        exclude_fields = ('authority_start', 'authority_verify')
-        interfaces =(relay.Node,)
+        exclude_fields = ("authority_start", "authority_verify")
+        interfaces = (relay.Node,)
+
 
 class TransactionFilter(django_filters.FilterSet):
-    status = django_filters.CharFilter(lookup_expr='icontains')
-    order_number = django_filters.CharFilter(lookup_expr='icontains')
+    status = django_filters.CharFilter(lookup_expr="icontains")
+    order_number = django_filters.CharFilter(lookup_expr="icontains")
     created_at_after = django_filters.DateFilter(
-        field_name='created_at',
-        lookup_expr='gt'
+        field_name="created_at", lookup_expr="gt"
     )
     created_at_before = django_filters.DateFilter(
-        field_name='created_at',
-        lookup_expr='lt'
+        field_name="created_at", lookup_expr="lt"
     )
     successful_payament_date_time_after = django_filters.DateFilter(
-        field_name='successful_payament_date_time',
-        lookup_expr='gt'
+        field_name="successful_payament_date_time", lookup_expr="gt"
     )
     successful_payament_date_time_before = django_filters.DateFilter(
-        field_name='successful_payament_date_time',
-        lookup_expr='lt'
+        field_name="successful_payament_date_time", lookup_expr="lt"
     )
-    failure_reason = django_filters.CharFilter(lookup_expr='icontains')   
+    failure_reason = django_filters.CharFilter(lookup_expr="icontains")
 
     class Meta:
         model = Transaction
-        fields = [
-            'status',
-            'order_number',
-            'failure_reason'
-        ]
+        fields = ["status", "order_number", "failure_reason"]
 
     @property
     def qs(self):
         return super().qs.filter(user=self.request.user)
 
 
-class YekpayQuery():
-    transactions = DjangoFilterConnectionField(TransactionNode, filterset_class=TransactionFilter)
+class YekpayQuery:
+    transactions = DjangoFilterConnectionField(
+        TransactionNode, filterset_class=TransactionFilter
+    )
 
 
 class UserFields(graphene.InputObjectType):
@@ -74,27 +66,3 @@ class TransactionFields(graphene.InputObjectType):
     description = graphene.String(required=True)
     fromCurrencyCode = graphene.String(required=True)
     toCurrencyCode = graphene.String(required=True)
-
-
-# class CreateTransaction(graphene.ClientIDMutation):
-#     class Arguments:
-#         transaction_input = graphene.Field(TransactionFields)
-#         person_input = graphene.Field(UserFields)
-#         address_input = graphene.Field(AddressFields)
-#         simulation = graphene.Boolean(required=False)
-#     redirect_url = graphene.String(required=True)
-#     order_number = graphene.Int(required=True)
-
-#     def mutate(self, info, transaction_input, person_input,address_input, simulation):
-#         if simulation is not False:
-#             transaction = yekpay_start_transaction(
-#                 request_function=request_yekpay_start_simulation,
-#                 **transaction_input
-#             )
-#         else:
-#             transaction = yekpay_start_transaction(**transaction_input)
-#         # get user info
-#         return CreateTransaction(
-#             redirect_url=transaction.get_transaction_start_url(),
-#             order_number=transaction.order_number.hashid
-#         )
