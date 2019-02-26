628d0e633dfaff81a4e5398eca3959f80703b132
glyphack <sh.hooshyari@gmail.com>
2019-02-26 16:15:40 +0330

    [yekpay]: refactored PEP8

--- yekpay/views.py
+++ yekpay/views.py
@@ -5,54 +5,42 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Transaction
 from .utils import generate_random_authority, process_transaction_trans_status
-from .request_utils import request_yekpay_verify,request_yekpay_verify_simulation
-from .config import YEKPAY_SIMULATION,MERCHANTID,FAILURE_REASONS
+from .request_utils import request_yekpay_verify, request_yekpay_verify_simulation
+from .config import YEKPAY_SIMULATION, MERCHANTID, FAILURE_REASONS
 from . import signals as yekpay_signals
 
 
 def sandbox_pay(request, authority_start):
-    if request.method == 'GET':
-        return render(
-            request,
-            'yekpay/sand-box.html'
-        )
-    elif request.method == 'POST':
-        transaction = Transaction.objects.get(authority_start= authority_start)
-        transaction_status = request.POST['status']
-        if transaction_status == 'SUCCESS':
+    if request.method == "GET":
+        return render(request, "yekpay/sand-box.html")
+    elif request.method == "POST":
+        transaction = Transaction.objects.get(authority_start=authority_start)
+        transaction_status = request.POST["status"]
+        if transaction_status == "SUCCESS":
             transaction.success()
-        elif transaction_status == 'FAILED':
-            random_failure_reason = FAILURE_REASONS[random.randint(0,len(FAILURE_REASONS))]
+        elif transaction_status == "FAILED":
+            random_failure_reason = FAILURE_REASONS[
+                random.randint(0, len(FAILURE_REASONS))
+            ]
             transaction.fail(random_failure_reason)
         transaction.authority_verify = generate_random_authority()
-        transaction.save(update_fields=['authority_verify'])
-        return redirect(
-            transaction.get_verify_url()
-        )
+        transaction.save(update_fields=["authority_verify"])
+        return redirect(transaction.get_verify_url())
 
 
-def verify_transaction_view(request,transaction_order_number,request_function=request_yekpay_verify):
+def verify_transaction_view(
+    request, transaction_order_number, request_function=request_yekpay_verify
+):
     if YEKPAY_SIMULATION:
         request_function = request_yekpay_verify_simulation
-    transaction = get_object_or_404(
-        Transaction,
-        order_number=transaction_order_number
-    )
+    transaction = get_object_or_404(Transaction, order_number=transaction_order_number)
     trans_status = request_function(
-        data=  {
-            "merchantId": MERCHANTID,
-            "authority": request.GET['authority']
-        }
+        data={"merchantId": MERCHANTID, "authority": request.GET["authority"]}
     )
-    transaction = process_transaction_trans_status(
-        transaction,
-        trans_status
-    )
-    yekpay_signals.transaction_verified.send(
-        sender=None,
-        transaction=transaction
-    )
+    transaction = process_transaction_trans_status(transaction, trans_status)
+    yekpay_signals.transaction_verified.send(sender=None, transaction=transaction)
     return redirect(transaction.get_client_callback_url())
+
 
 # class transactionsDetailView(DetailView):
 #     model = transactions
