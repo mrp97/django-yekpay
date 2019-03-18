66421f8b4a4734efcd24ae346822b5665d696414
glyphack <sh.hooshyari@gmail.com>
2019-03-18 16:39:01 +0330

    [yekpay]: updated snapshots

---
+++ yekpay/tests/snapshots/snap_test_commands.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['CommandsTestCase::test_export_container_numbers_without_arguments 1'] = ''
