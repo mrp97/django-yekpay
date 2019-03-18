ac6d1c7facb119fd7f8a67b6e8be288a0f673418
glyphack <sh.hooshyari@gmail.com>
2019-03-18 16:33:14 +0330

    [yekpay]: removed unused urls.

--- yekpay/urls.py
+++ yekpay/urls.py
@@ -8,31 +8,6 @@ from . import views
 
 app_name = 'yekpay'
 urlpatterns = [
-    # url(
-    #     regex="^transactions/~create/$",
-    #     view=views.yekpay_start_transaction(transaction_data),
-    #     name='transactions_create',
-    # ),
-    # url(
-    #     regex="^transactions/~verify/$",
-    #     view=views.yekpay_proccess_transaction(),
-    #     name='transactions_verify',
-    # ),
-    # url(
-    #     regex="^transactions/(?P<pk>\d+)/$",
-    #     view=views.transactionsDetailView.as_view(),
-    #     name='transactions_detail',
-    # ),
-    # url(
-    #     regex="^transactions/(?P<pk>\d+)/~update/$",
-    #     view=views.transactionsUpdateView.as_view(),
-    #     name='transactions_update',
-    # ),
-    # url(
-    #     regex="^transactions/$",
-    #     view=views.transactionsListView.as_view(),
-    #     name='transactions_list',
-    # ),
     path('sanbox-transaction/<int:authority_start>/',
          views.sandbox_pay,
          name='sandbox-payment'
